0218507fb681a64ba3a32e91599c4051c2e9c4b2
Roy Bae <roysbae@gmail.com>
2024-11-18 09:53:14 -0800

    added gravity to character movement and updated drawing logic

--- script.js
+++ script.js
@@ -15,25 +15,38 @@ canvas.height = height;
 // fill our canvas with the fillRect method taking in 4 arguments (x, y, width, height)
 c.fillRect(0, 0, width, height);
 
+// introduce gravity to our game
+const gravity = 0.2;
+
 /*Character Objcet*/
 // use OOP because we are going to need to have characters interact
 class Sprite {
     // in constructor assign the passed in argument position to the sprits(this) position
-    // *little trick if you start to get more and more arguments you have to be conscious of what you passin and the order. To comat this just wrap the arguments in an object and your life will be alot simpler
+    // *little trick if you start to get more and more arguments you have to be conscious of what you passin and the order. To combat this just wrap the arguments in an object and your life will be alot simpler
     constructor({ position, velocity }) {
         // set the passed in position to the classes instance property
         this.position = position;
-        //
+        // set the passed in velocity to the classes instance property
         this.velocity = velocity;
-        // invoke the instance method draw to make sure we don't have to call it every we create a new character.
-        this.draw();
+        // set the height of the character so we know when to stop falling
+        this.height = 150;
     }
     // allows us to draw the characters with the classes positions
     draw() {
         // style the box first, can be any color, red for now
         c.fillStyle = "red";
         // fill in the rect with the classes positions and give it some width and height
-        c.fillRect(this.position.x, this.position.y, 50, 150);
+        c.fillRect(this.position.x, this.position.y, 50, this.height);
+    }
+    // update the characters postion and velocity to simulate gravity
+    update() {
+        this.draw();
+        this.position.y += this.velocity.y;
+        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+            this.velocity.y = 0;
+        } else {
+            this.velocity.y += gravity;
+        }
     }
 }
 
@@ -69,6 +82,13 @@ const animate = () => {
     window.requestAnimationFrame(animate);
     // uncomment this to test if animation is working
     // console.log("ping");
+    // fill the canvas back to black
+    c.fillStyle = "black";
+    // this gets rid of the previous frames of the characters
+    c.fillRect(0, 0, width, height);
+    // update the characters positions
+    player1.update();
+    player2.update();
 };
 // make sure to invoke the function, defining a function will not invoke a function
 animate();
