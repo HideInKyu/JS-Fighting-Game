cd6bb138a462b16aae76a9b7f84a596764ed755b
Roy Bae <roysbae@gmail.com>
2024-11-18 11:28:33 -0800

    Made player one hitbox hide and show properly with a consolelog to verify if we hit the enemy or not

--- script.js
+++ script.js
@@ -1,178 +1,221 @@
-/*View port*/
-// store the canvas element from our HTML file into a const for easier select
+/* Viewport */
+// Store the canvas element from our HTML file into a constant for easier selection
 const canvas = document.querySelector("canvas");
-// use get the get context method with argument 2d to allow us to draw in our canvas
+// Use the getContext method with argument "2d" to allow us to draw in our canvas
 const c = canvas.getContext("2d");
 
-// assign width and height to have better control of screen sizes
+// Assign width and height to have better control of screen sizes
 const width = 1024;
 const height = 524;
 
-// assign the width and height of the canvas
+// Assign the width and height of the canvas
 canvas.width = width;
 canvas.height = height;
 
-// fill our canvas with the fillRect method taking in 4 arguments(x, y, width, height)
+// Fill our canvas with the fillRect method taking in 4 arguments (x, y, width, height)
 c.fillRect(0, 0, width, height);
 
-// introduce gravity to our game
+// Introduce gravity to our game
 const gravity = 0.2;
-/*Character Objcet*/
-// use OOP because we are going to need to have characters interact
+
+/* Character Object */
+// Use OOP because we are going to need to have characters interact
 class Sprite {
-    // in constructor assign the passed in argument position to the sprits(this) position
-    // *little trick if you start to get more and more arguments you have to be conscious of what you passin and the order. To combat this just wrap the arguments in an object and your life will be alot simpler
+    // In constructor, assign the passed-in argument position to the sprite's (this) position
     constructor({ position, velocity, keys, color }) {
-        // set the passed in position to the classes instance property
+        // Set the passed-in position to the class's instance property
         this.position = position;
-        // set the passed in velocity to the classes instance property
+        // Set the passed-in velocity to the class's instance property
         this.velocity = velocity;
-        // set the height of the character so we know when to stop falling
+        // Set the height of the character so we know when to stop falling
         this.height = 150;
+        // Set the width of the character
+        this.width = 50;
+        // Store the keys for movement
         this.keys = keys;
+        // Define the attack box for collision detection
         this.attackBox = {
-            position: this.position,
-            width: 100,
-            height: 50,
+            position: this.position, // The attack box moves with the character
+            width: 100, // Width of the attack box
+            height: 50, // Height of the attack box
         };
+        // Set the color of the character
         this.color = color;
+        // Set the character to not attacking by default
+        this.isAttacking = false;
     }
-    // allows us to draw the characters with the classes positions
+
+    // Allows us to draw the characters with the class's positions
     draw() {
-        // style the box first, can be any color, red for now
+        // Style the box first, can be any color, red for now
         c.fillStyle = this.color;
-        // fill in the rect with the classes positions and give it some width and height
-        c.fillRect(this.position.x, this.position.y, 50, this.height);
-        c.fillStyle = "green";
-        c.fillRect(
-            this.attackBox.position.x,
-            this.attackBox.position.y,
-            this.attackBox.width,
-            this.attackBox.height,
-        );
+        // Fill in the rect with the class's positions and give it some width and height
+        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+        // Draw the attack box
+        if (this.isAttacking) {
+            c.fillStyle = "green";
+            c.fillRect(
+                this.attackBox.position.x,
+                this.attackBox.position.y,
+                this.attackBox.width,
+                this.attackBox.height,
+            );
+        }
     }
-    // update the characters postion and velocity to simulate gravity
+
+    // Update the character's position and velocity to simulate gravity
     update() {
         this.draw();
         this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
+        // Check if the character is touching the ground
         if (this.position.y + this.height + this.velocity.y >= canvas.height) {
-            this.velocity.y = 0;
+            this.velocity.y = 0; // Stop downward movement if touching the ground
         } else {
-            this.velocity.y += gravity;
+            this.velocity.y += gravity; // Apply gravity if not touching the ground
         }
+    }
+
+    attack() {
+        this.isAttacking = true;
+        setTimeout(() => {
+            this.isAttacking = false;
+        }, 100);
     }
 }
 
-// create player 1 with really any x or y position using an object
-// *because of the trick we have to refactor our sprites, its easy, just wrap the arguments in an object and then assign what is needed
+// Create player 1 with initial position, velocity, keys, and color
 const player1 = new Sprite({
     position: {
-        x: 0,
-        y: 0,
+        x: 0, // Initial x position of player 1
+        y: 0, // Initial y position of player 1
     },
-    // set velocity x and y to 0, as a default our characters won't have any inharent speed/velocity and will be effected by other things later like gravity
+    // Set initial velocity x and y to 0, meaning the character is stationary at the start
     velocity: {
-        x: 0,
-        y: 0,
+        x: 0, // Initial x velocity of player 1
+        y: 0, // Initial y velocity of player 1
     },
+    // Define the keys used to control player 1
     keys: {
-        w: 0,
-        a: false,
-        d: false,
+        w: 0, // Key for jumping
+        a: false, // Key for moving left
+        d: false, // Key for moving right
     },
-    color: "red",
-});
-// create player 2 with really any x or y position using an object
-// *here as well
-const player2 = new Sprite({
-    position: {
-        x: 900,
-        y: 0,
-    },
-    velocity: {
-        x: 0,
-        y: 0,
-    },
-    keys: {
-        ArrowUp: 0,
-        ArrowLeft: false,
-        ArrowRight: false,
-    },
-    color: "blue",
+    color: "red", // Color of player 1
 });
 
+// Create player 2 with initial position, velocity, keys, and color
+const player2 = new Sprite({
+    position: {
+        x: 900, // Initial x position of player 2
+        y: 0, // Initial y position of player 2
+    },
+    // Set initial velocity x and y to 0, meaning the character is stationary at the start
+    velocity: {
+        x: 0, // Initial x velocity of player 2
+        y: 0, // Initial y velocity of player 2
+    },
+    // Define the keys used to control player 2
+    keys: {
+        ArrowUp: 0, // Key for jumping
+        ArrowLeft: false, // Key for moving left
+        ArrowRight: false, // Key for moving right
+    },
+    color: "blue", // Color of player 2
+});
+
+// Function to check if the player is touching the ground
 const isTouchingGround = (player) => {
+    // If the player's bottom edge is at or below the canvas height, they are touching the ground
     if (player.position.y + player.height >= canvas.height) {
         return true;
     }
     return false;
 };
 
+// Function to handle player movement
 const playerMovement = (player) => {
     window.addEventListener("keydown", (e) => {
-        console.log(
-            e.key,
-            Object.keys(player.keys)[0],
-            Object.keys(player.keys)[1],
-            Object.keys(player.keys)[2],
-        );
+        // Check for jump key and if the player is touching the ground
         if (e.key === Object.keys(player.keys)[0] && isTouchingGround(player)) {
             player.velocity.y = -10;
         }
+        // Check for left movement key
         if (e.key === Object.keys(player.keys)[1]) {
-            player.keys[Object.keys(player.keys)[1]] = true;
+            player.keys[Object.keys(player.keys)[1]] = true; // Set left movement key to true
         }
+        // Check for right movement key
         if (e.key === Object.keys(player.keys)[2]) {
-            player.keys[Object.keys(player.keys)[2]] = true;
+            player.keys[Object.keys(player.keys)[2]] = true; // Set right movement key to true
+        }
+        // Check for attack key
+        if (e.key === " ") {
+            player.attack();
         }
     });
 
     window.addEventListener("keyup", (e) => {
+        // Stop movement when the key is released
         switch (e.key) {
             case Object.keys(player.keys)[1]:
-                player.keys[Object.keys(player.keys)[1]] = false;
+                player.keys[Object.keys(player.keys)[1]] = false; // Set left movement key to false
                 break;
             case Object.keys(player.keys)[2]:
-                player.keys[Object.keys(player.keys)[2]] = false;
+                player.keys[Object.keys(player.keys)[2]] = false; // Set right movement key to false
                 break;
         }
     });
 };
 
+// Function to update player velocity based on key presses
 const updatePlayerVelocity = (player) => {
     if (
         player.keys[Object.keys(player.keys)[1]] &&
         player.keys[Object.keys(player.keys)[2]]
     ) {
-        player.velocity.x = 0;
+        player.velocity.x = 0; // If both left and right keys are pressed, stop horizontal movement
     } else if (player.keys[Object.keys(player.keys)[1]]) {
-        player.velocity.x = -5;
+        player.velocity.x = -5; // Move left
     } else if (player.keys[Object.keys(player.keys)[2]]) {
-        player.velocity.x = 5;
+        player.velocity.x = 5; // Move right
     } else {
-        player.velocity.x = 0;
+        player.velocity.x = 0; // Stop horizontal movement if no keys are pressed
     }
 };
 
+// Initialize player movement for both players
 playerMovement(player1);
 playerMovement(player2);
 
-/* AnimationLoop */
+// Function to check if two players are touching (collision detection)
+const isTouching = (p1, p2) => {
+    if (
+        p1.attackBox.position.x + p1.attackBox.width >= p2.position.x &&
+        p1.attackBox.position.x <= p2.position.x + p2.width &&
+        p1.attackBox.position.y + p1.attackBox.height >= p2.position.y &&
+        p1.attackBox.position.y <= p2.position.y + p2.height &&
+        p1.isAttacking
+    ) {
+        p1.isAttacking = false;
+        console.log("hit"); // Log a message if the players are touching
+    }
+};
+
+/* Animation Loop */
 const animate = () => {
-    // request animation frames and recursively call animate to continually "infinetly" create animation frames
+    // Request animation frames and recursively call animate to continually create animation frames
     window.requestAnimationFrame(animate);
-    // uncomment this to test if animation is working
-    // console.log("ping");
-    // fill the canvas back to black
+    // Fill the canvas back to black
     c.fillStyle = "black";
-    // this gets rid of the previous frames of the characters
+    // This gets rid of the previous frames of the characters
     c.fillRect(0, 0, width, height);
-    // update the characters positions
+    // Update the characters' positions
     updatePlayerVelocity(player1);
     updatePlayerVelocity(player2);
     player1.update();
     player2.update();
+    // Check for collisions
+    isTouching(player1, player2);
 };
-// make sure to invoke the function, defining a function will not invoke a function
+// Invoke the function to start the animation loop
 animate();
