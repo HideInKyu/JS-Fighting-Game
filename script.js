1d6e6c2008411d4be1346ae2349fbc308781b16c
Roy Bae <roysbae@gmail.com>
2024-11-18 11:08:07 -0800

    added perfect movement into the game, and made color customizeable with a basic hitbox that shows

--- script.js
+++ script.js
@@ -12,35 +12,49 @@ const height = 524;
 canvas.width = width;
 canvas.height = height;
 
-// fill our canvas with the fillRect method taking in 4 arguments (x, y, width, height)
+// fill our canvas with the fillRect method taking in 4 arguments(x, y, width, height)
 c.fillRect(0, 0, width, height);
 
 // introduce gravity to our game
 const gravity = 0.2;
-
 /*Character Objcet*/
 // use OOP because we are going to need to have characters interact
 class Sprite {
     // in constructor assign the passed in argument position to the sprits(this) position
     // *little trick if you start to get more and more arguments you have to be conscious of what you passin and the order. To combat this just wrap the arguments in an object and your life will be alot simpler
-    constructor({ position, velocity }) {
+    constructor({ position, velocity, keys, color }) {
         // set the passed in position to the classes instance property
         this.position = position;
         // set the passed in velocity to the classes instance property
         this.velocity = velocity;
         // set the height of the character so we know when to stop falling
         this.height = 150;
+        this.keys = keys;
+        this.attackBox = {
+            position: this.position,
+            width: 100,
+            height: 50,
+        };
+        this.color = color;
     }
     // allows us to draw the characters with the classes positions
     draw() {
         // style the box first, can be any color, red for now
-        c.fillStyle = "red";
+        c.fillStyle = this.color;
         // fill in the rect with the classes positions and give it some width and height
         c.fillRect(this.position.x, this.position.y, 50, this.height);
+        c.fillStyle = "green";
+        c.fillRect(
+            this.attackBox.position.x,
+            this.attackBox.position.y,
+            this.attackBox.width,
+            this.attackBox.height,
+        );
     }
     // update the characters postion and velocity to simulate gravity
     update() {
         this.draw();
+        this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
         if (this.position.y + this.height + this.velocity.y >= canvas.height) {
             this.velocity.y = 0;
@@ -62,6 +76,12 @@ const player1 = new Sprite({
         x: 0,
         y: 0,
     },
+    keys: {
+        w: 0,
+        a: false,
+        d: false,
+    },
+    color: "red",
 });
 // create player 2 with really any x or y position using an object
 // *here as well
@@ -74,7 +94,69 @@ const player2 = new Sprite({
         x: 0,
         y: 0,
     },
+    keys: {
+        ArrowUp: 0,
+        ArrowLeft: false,
+        ArrowRight: false,
+    },
+    color: "blue",
 });
+
+const isTouchingGround = (player) => {
+    if (player.position.y + player.height >= canvas.height) {
+        return true;
+    }
+    return false;
+};
+
+const playerMovement = (player) => {
+    window.addEventListener("keydown", (e) => {
+        console.log(
+            e.key,
+            Object.keys(player.keys)[0],
+            Object.keys(player.keys)[1],
+            Object.keys(player.keys)[2],
+        );
+        if (e.key === Object.keys(player.keys)[0] && isTouchingGround(player)) {
+            player.velocity.y = -10;
+        }
+        if (e.key === Object.keys(player.keys)[1]) {
+            player.keys[Object.keys(player.keys)[1]] = true;
+        }
+        if (e.key === Object.keys(player.keys)[2]) {
+            player.keys[Object.keys(player.keys)[2]] = true;
+        }
+    });
+
+    window.addEventListener("keyup", (e) => {
+        switch (e.key) {
+            case Object.keys(player.keys)[1]:
+                player.keys[Object.keys(player.keys)[1]] = false;
+                break;
+            case Object.keys(player.keys)[2]:
+                player.keys[Object.keys(player.keys)[2]] = false;
+                break;
+        }
+    });
+};
+
+const updatePlayerVelocity = (player) => {
+    if (
+        player.keys[Object.keys(player.keys)[1]] &&
+        player.keys[Object.keys(player.keys)[2]]
+    ) {
+        player.velocity.x = 0;
+    } else if (player.keys[Object.keys(player.keys)[1]]) {
+        player.velocity.x = -5;
+    } else if (player.keys[Object.keys(player.keys)[2]]) {
+        player.velocity.x = 5;
+    } else {
+        player.velocity.x = 0;
+    }
+};
+
+playerMovement(player1);
+playerMovement(player2);
 
 /* AnimationLoop */
 const animate = () => {
@@ -87,6 +169,8 @@ const animate = () => {
     // this gets rid of the previous frames of the characters
     c.fillRect(0, 0, width, height);
     // update the characters positions
+    updatePlayerVelocity(player1);
+    updatePlayerVelocity(player2);
     player1.update();
     player2.update();
 };
